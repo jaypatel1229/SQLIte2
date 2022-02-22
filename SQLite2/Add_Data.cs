32a4aa7136838614d5af37c1b551a08397271af9
jay.patel <jay.patel@1R-80>
2022-02-22 17:51:27 +0530

    Add project files.

---
+++ SQLite2/Add_Data.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLite2
+{
+    [Activity(Label = "Add_Data")]
+    public class Add_Data : Activity
+    {
+        EditText txt;
+        Button btn;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Add_Data);
+
+            UIReferences();
+            UIClick();
+            
+        }
+
+        private void UIReferences()
+        {
+            txt = FindViewById<EditText>(Resource.Id.edtText);
+            btn = FindViewById<Button>(Resource.Id.addBtn);
+        }
+
+        private void UIClick()
+        {
+            btn.Click += AddBtn_Click;
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(activity_main));
+            StartActivity(intent);
+        }
+    }
+}
---
+++ SQLite2/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace SQLite2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Toolbar toolBar;
+        ImageView imgView;
+        ListView listView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            UIReferences();
+            UIClick();
+        }
+
+        private void UIReferences()
+        {
+            toolBar = FindViewById<Toolbar>(Resource.Id.toolbar1);
+            imgView = FindViewById<ImageView>(Resource.Id.imgBtn);
+            listView = FindViewById<ListView>(Resource.Id.listView1);
+        }
+
+        private void UIClick()
+        {
+            imgView.Click += delegate{
+                Intent i = new Intent(this, typeof(Add_Data));
+                StartActivity(i);
+            };
+        }       
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
